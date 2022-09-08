d7f64665283cdb3555c5b1c3e7a6fdfc339b37e7
jsprolsson <jesper.p.olsson@hotmail.com>
2022-09-08 15:38:21 +0200

    added func for recentlyaddedconcerts

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import "../styles/HomeComponent.css";
 import TodaysShowsComponent from "./TodaysShowsComponent";
+import RecentlyAddedConcertsComponent from "./RecentlyAddedConcertsComponent";
 
 function HomeComponent() {
   return (
@@ -9,6 +10,7 @@ function HomeComponent() {
         <img src="src/assets/Livefanatic.png" alt="" />
       </div>
       <div>
+        <RecentlyAddedConcertsComponent/>
         <TodaysShowsComponent />
       </div>
     </div>
