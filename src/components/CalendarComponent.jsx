8989080397ae562d1a8e0def1bc7f1d024c8718a
Filip Nicklasson <Filip@nicklasson.nu>
2022-09-10 15:41:42 +0200

    Tickets-available
    You can now see if there are sold out tickets, few tickets or alot tickets available. And i put a color on them so its more clear to see

--- src/components/CalendarComponent.jsx
+++ src/components/CalendarComponent.jsx
@@ -7,14 +7,14 @@ const CalendarComponent = () => {
       artist: "Deafhaven",
       location: "Göteborg",
       date: "04-06-2022",
-      numberOfTickets: 50,
+      numberOfTickets: 62547,
     },
     {
       id: 2,
       artist: "King Krule",
       location: "Stockholm",
       date: "04-06-2022",
-      numberOfTickets: 1000,
+      numberOfTickets: 140,
     },
     {
       id: 3,
@@ -34,7 +34,7 @@ const CalendarComponent = () => {
             <div className="info">
               <div className="artist">{data.artist}</div>
               <div className="location">{data.location}</div>
-              <div className="Tickets">{data.numberOfTickets === 0? <span>Sold Out</span>: <span>Tickets Available</span>}</div>
+              <div className="Tickets">{data.numberOfTickets === 0? <span id='Sold-out'>Sold Out</span>: data.numberOfTickets <= 150? <span id='Few-tickets'>Few tickets Available</span>: <span id='Available-tickets'>A Lot Tickets Available</span>}</div>
             </div>
             <span className="material-symbols-outlined">arrow_forward</span>
           </div>
