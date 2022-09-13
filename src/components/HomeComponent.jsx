382c79ca44606e9cfb85b108e60a8897e3d8db98
jonathandepartment <68937234+jonathandepartment@users.noreply.github.com>
2022-09-13 15:00:42 +0200

    hooked up events to backend for the frontpage
    * Pass events to recently added as props

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,17 +1,28 @@
-import { Link } from "react-router-dom";
 import "../styles/HomeComponent.css";
 import TodaysShowsComponent from "./TodaysShowsComponent";
 import RecentlyAddedConcertsComponent from "./RecentlyAddedConcertsComponent";
-
+import { useState, useEffect } from 'react'
 
 function HomeComponent() {
+  const [events, setEvents] = useState([])
+  useEffect(() => {
+    const loadData = async () => {
+      let data = await fetch('/data/events')
+      let json = await data.json()
+
+      setEvents(json)
+    }
+
+    loadData()
+  }, [])
+
   return (
     <div className="main">
       <div className="mainpic">
         <img src="src/assets/Livefanatic.png" alt="" />
       </div>
       <div>
-        <RecentlyAddedConcertsComponent/>
+        <RecentlyAddedConcertsComponent events={events} />
         <TodaysShowsComponent />
       </div>
     </div>
