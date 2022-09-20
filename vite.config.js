de3d92b1e84302be1b2670bac00ff7a6f8b76529
Filip Nicklasson <Filip@nicklasson.nu>
2022-09-20 10:13:37 +0200

    vite-update
    newer verision on my vite so forcing 127.0.0.1, from vite verision 17, they let us run on localhost.

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '127.0.0.1',
     proxy: {
       '/data': 'http://localhost:3333'
     }
