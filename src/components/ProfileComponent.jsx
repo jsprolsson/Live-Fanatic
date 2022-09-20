3287409d3b284a04a6fa820b74fbddb51ed4256e
alexurtti <92301547+alexurtti@users.noreply.github.com>
2022-09-20 11:18:27 +0200

    Log out mode in mobile mode
    Added log out log out function to button in profile and added "upcoming month" text to home page

--- src/components/ProfileComponent.jsx
+++ src/components/ProfileComponent.jsx
@@ -8,6 +8,7 @@ import { useStore } from "../store/useStore";
 import ModalComponent from "./ModalComponent";
 import LoginComponent from "./LogInComponent";
 import UserTicketsProfileComponent from "./UserTicketsProfileComponent";
+import userService from "../services/userservice";
 
 function ProfileComponent() {
   const [userFromDb, setUserFromDb] = useState({});
@@ -18,51 +19,55 @@ function ProfileComponent() {
   const [error, setError] = useState("");
   const { user, setUser } = useStore();
   const [show, setShow] = useState(false);
-  
+  const navigate = useNavigate();
 
   //hämta användare
   useEffect(() => {
-    let isCancelled = false
+    let isCancelled = false;
     const fetchData = async () => {
-      if(!isCancelled) {
-      let response = await fetch("/data/login");
-      let userData = await response.json();
+      if (!isCancelled) {
+        let response = await fetch("/data/login");
+        let userData = await response.json();
 
-      setUserFromDb(userData);
+        setUserFromDb(userData);
 
-      //hämta user tickets
-      const userTicketData = await paymentService.getTickets(userData.id);
-      setUserTickets(userTicketData);
+        //hämta user tickets
+        const userTicketData = await paymentService.getTickets(userData.id);
+        setUserTickets(userTicketData);
 
-      //filtrera ut bara ticketsid till egen lista
-      const ticketsIds = userTicketData.map(({ event_id }) => event_id);
+        //filtrera ut bara ticketsid till egen lista
+        const ticketsIds = userTicketData.map(({ event_id }) => event_id);
 
-      //fetch alla events
-      const allEvents = await eventService.getAll();
+        //fetch alla events
+        const allEvents = await eventService.getAll();
 
-      //filtrera ut bara events usern ska på och sortera efter datum
-      let filteredEventsList = allEvents.filter((event) =>
-        ticketsIds.includes(event.id)
-      ).sort((a,b) => {
-          return a.date > b.date ? 1 : -1;
-      })
-      
-      // lägg till utgånga biljetter i egen lista
-      let expiredTickets = filteredEventsList.filter(event => Date.parse(event.date) < new Date()).splice(-3)
+        //filtrera ut bara events usern ska på och sortera efter datum
+        let filteredEventsList = allEvents
+          .filter((event) => ticketsIds.includes(event.id))
+          .sort((a, b) => {
+            return a.date > b.date ? 1 : -1;
+          });
 
-      //ta bort utgånga biljetter från listan med userns event
-      filteredEventsList = filteredEventsList.filter(event => Date.parse(event.date) > new Date())
+        // lägg till utgånga biljetter i egen lista
+        let expiredTickets = filteredEventsList
+          .filter((event) => Date.parse(event.date) < new Date())
+          .splice(-3);
 
-      //lägg till utgånga events i slutet av listan för att separera aktuella och utgånga biljetter
-      const sortedUsersEventList = filteredEventsList.concat(expiredTickets)
+        //ta bort utgånga biljetter från listan med userns event
+        filteredEventsList = filteredEventsList.filter(
+          (event) => Date.parse(event.date) > new Date()
+        );
 
-      setUserEvents(sortedUsersEventList);
-    }
+        //lägg till utgånga events i slutet av listan för att separera aktuella och utgånga biljetter
+        const sortedUsersEventList = filteredEventsList.concat(expiredTickets);
+
+        setUserEvents(sortedUsersEventList);
+      }
     };
     fetchData();
     return () => {
-      isCancelled = true
-    }
+      isCancelled = true;
+    };
   }, []);
 
   function Message(message) {
@@ -76,9 +81,16 @@ function ProfileComponent() {
     e.preventDefault();
     changePassword(newPassword);
   };
-  const seeTicket=()=>{
-    navigate ("/ticketComponent")
-  }
+
+  const login = async () => {
+    setUser(null);
+    navigate("/");
+    await userService.logout();
+  };
+
+  const seeTicket = () => {
+    navigate("/ticketComponent");
+  };
 
   const changePassword = async (newPassword) => {
     // //sätter lösenord till null i db
@@ -133,49 +145,53 @@ function ProfileComponent() {
             <h1 className="profile-h1">Profile</h1>
           </div>
           <div className="profile-page">
-            <form id="register-form" onSubmit={handleSubmit}>
-              <div className="account-label">
-                <h2 className="profile-h2">Update Password on your Account</h2>
-              </div>
+            <div className="register-details">
+              <form id="register-form" onSubmit={handleSubmit}>
+                <div className="account-label">
+                  <h2 className="profile-h2">
+                    Update Password on your Account
+                  </h2>
+                </div>
 
-
-              <label className="profile-label">
-                Enter Email
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  value={email}
-                  onChange={(e) => setUpdateEmail(e.target.value)}
-                  placeholder="email"
-                ></input>
-              </label>
-              <label className="profile-label" htmlFor="confirmPassword">
-                {" "}
-                Enter New Password
-                <input
-                  type="password"
-                  name="newPassword"
-                  id="newPassword"
-                  value={newPassword}
-                  onChange={(e) => setUpdateNewPassword(e.target.value)}
-                  placeholder="New Password"
-                ></input>
-              </label>
-              <button className="button" type="submit">
-                Save Changes
-              </button>
+                <label className="profile-label">
+                  Enter Email
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setUpdateEmail(e.target.value)}
+                    placeholder="email"
+                  ></input>
+                </label>
+                <label className="profile-label" htmlFor="confirmPassword">
+                  {" "}
+                  Enter New Password
+                  <input
+                    type="password"
+                    name="newPassword"
+                    id="newPassword"
+                    value={newPassword}
+                    onChange={(e) => setUpdateNewPassword(e.target.value)}
+                    placeholder="New Password"
+                  ></input>
+                </label>
+                <button className="button" type="submit">
+                  Save Changes
+                </button>
+                <div className="profile-error">{error}</div>
+              </form>
               <div className="logout-button-component">
-                <button className="button">Log out</button>
+                <button className="button" onClick={login}>
+                  Log out
+                </button>
               </div>
-              <div className="profile-error">{error}</div>
-            </form>
-
+            </div>
             <div className="user-purchase-list">
               <h1 className="profile-h1">Ticket Purchase</h1>
               <div className="concert-list">
                 {userevents.map((events) => (
-                  <UserTicketsProfileComponent key={events.id} event={events}/>
+                  <UserTicketsProfileComponent key={events.id} event={events} />
                 ))}
               </div>
             </div>
@@ -186,8 +202,14 @@ function ProfileComponent() {
           <div className="profile-unauthorized-page">
             <h1>You are not logged in</h1>
             <span>
-              Please log in <span className="profile-login-link" onClick={() => setShow(true)}>here</span> to
-              see tour profile
+              Please log in{" "}
+              <span
+                className="profile-login-link"
+                onClick={() => setShow(true)}
+              >
+                here
+              </span>{" "}
+              to see tour profile
             </span>
           </div>
           <ModalComponent
