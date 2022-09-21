d87c8e47424894789591171b76d4576b96ef0ca1
jonathandepartment <68937234+jonathandepartment@users.noreply.github.com>
2022-09-21 11:09:02 +0200

    fixed css and text on cancelbuy, hooked up buttons

--- src/components/CancelBuyComponent.jsx
+++ src/components/CancelBuyComponent.jsx
@@ -1,20 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import "../styles/CancelBuyComponent.css";
 
 const CancelBuyComponent = () => {
-    return(
-        <div className="Cancelbuy">
-            <span id="cancelbuySymbol" className="material-symbols-outlined">close</span>
-            <h2 id="cancelbuy-H3">You did not complete the purchase!<br/><br/>You will now be redirected to Home or Event</h2>
-            <div className="cancelbuy-position">  
-            <button id="cancelbuy-Homepagebtn">
-                HOME
-            </button>
-            <button id="cancelbuy-BackToEventpagebtn">
-                EVENT
-            </button>
-            </div>
-        </div>
-    );
+  const navigate = useNavigate()
+  return (
+    <div className="Cancelbuy">
+      <span id="cancelbuySymbol" className="material-symbols-outlined">close</span>
+      <h2 id="cancelbuy-H3">Purchase failed.</h2>
+      <div className="cancelbuy-position">
+        <button onClick={() => navigate('/')} className="cancelbuy-btn">
+          Home
+        </button>
+        <button onClick={() => navigate('/events')} className="cancelbuy-btn">
+          Events
+        </button>
+      </div>
+    </div>
+  );
 }
 
 
